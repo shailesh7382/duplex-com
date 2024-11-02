bfafffd2e1588885f9650d71bef1765c72789a58
Shailesh Singh <shailesh7382@gmail.com>
2024-11-02 15:26:06 +0800

    add related changes

--- duplex-client/src/main/java/com/shailesh/config/GrpcClientConfig.java
+++ duplex-client/src/main/java/com/shailesh/config/GrpcClientConfig.java
@@ -1,10 +1,14 @@
 package com.shailesh.config;
 
 import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.SSLException;
+import java.io.File;
 
 import com.example.shailesh.fxoption.FXOptionServiceGrpc;
 
@@ -17,11 +21,23 @@ public class GrpcClientConfig {
     @Value("${grpc.client.port}")
     private int grpcClientPort;
 
+    @Value("${grpc.client.certChainFilePath}")
+    private String certChainFilePath;
+
+    @Value("${grpc.client.privateKeyFilePath}")
+    private String privateKeyFilePath;
+
+    @Value("${grpc.client.trustCertCollectionFilePath}")
+    private String trustCertCollectionFilePath;
+
     @Bean
-    public ManagedChannel managedChannel() {
-        return ManagedChannelBuilder.forAddress(grpcClientHost, grpcClientPort)
-                                    .usePlaintext()
-                                    .build();
+    public ManagedChannel managedChannel() throws SSLException {
+        return NettyChannelBuilder.forAddress(grpcClientHost, grpcClientPort)
+                .sslContext(GrpcSslContexts.forClient()
+                        .keyManager(new File(certChainFilePath), new File(privateKeyFilePath))
+                        .trustManager(new File(trustCertCollectionFilePath))
+                        .build())
+                .build();
     }
 
     @Bean
--- duplex-server/src/main/java/com/shailesh/config/GrpcServerConfig.java
+++ duplex-server/src/main/java/com/shailesh/config/GrpcServerConfig.java
@@ -1,13 +1,15 @@
 package com.shailesh.config;
 
+import com.shailesh.service.FXOptionServiceImpl;
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.shailesh.service.FXOptionServiceImpl;
-
+import javax.net.ssl.SSLException;
+import java.io.File;
 import java.io.IOException;
 
 @Configuration
@@ -16,11 +18,30 @@ public class GrpcServerConfig {
     @Value("${grpc.server.port}")
     private int grpcServerPort;
 
+    @Value("${grpc.server.certChainFilePath}")
+    private String certChainFilePath;
+
+    @Value("${grpc.server.privateKeyFilePath}")
+    private String privateKeyFilePath;
+
+    @Value("${grpc.server.trustCertCollectionFilePath}")
+    private String trustCertCollectionFilePath;
+
     @Bean
-    public Server grpcServer(FXOptionServiceImpl fxOptionService) throws IOException {
-        return ServerBuilder.forPort(grpcServerPort)
-                .addService(fxOptionService)
-                .build()
-                .start();
+    public Server grpcServer(FXOptionServiceImpl fxOptionService) throws SSLException {
+        try {
+            return NettyServerBuilder.forPort(grpcServerPort)
+                    .addService(fxOptionService)
+                    .sslContext(GrpcSslContexts.forServer(new File(certChainFilePath), new File(privateKeyFilePath))
+                            .trustManager(new File(trustCertCollectionFilePath))
+                            .clientAuth(io.grpc.netty.shaded.io.netty.handler.ssl.ClientAuth.REQUIRE)
+                            .build())
+                    .build()
+                    .start();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+                return null;
     }
 }
